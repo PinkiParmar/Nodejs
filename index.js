3670d63429ff6aa49de8d83937c28b2c4d9c3824
Pinki Parmar <parmarpinki932@gmail.com>
2023-05-19 13:05:02 +0530

    first commit

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 app.get('/',(req,res) => {
     res.send('Welcome');
 });
+app.get('/node',(req,res) => {
+    res.send('Welcome to node');
+});
 app.listen(3001,() => {
     console.log('listening on');
 });
